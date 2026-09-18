3737118f3bc89510201ca82de408bffd61264ad8
Ed Studios <edstudios73@gmail.com>
2026-09-18 04:51:40 +0100

    Configure web push public key

--- js/push-config.js
+++ js/push-config.js
@@ -1,2 +1,2 @@
 // Set this to the public VAPID key generated for the Supabase notification function.
-window.CSC_PUSH_VAPID_PUBLIC_KEY = '';
+window.CSC_PUSH_VAPID_PUBLIC_KEY = 'BAWrMzkbKDmT0fc5D5kz_ZK6fTPRpYh4XKEQnrW2zax8C0_oavfL5_ZDKsywbrTRwpLOdtLBlyklP-CO_Fnqx8A';
